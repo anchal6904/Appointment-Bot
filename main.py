9a442d28ba8786ff8d3b082d2724d6178442a77a
Anchal Jain <anchal6904@gmail.com>
2025-06-28 11:56:46 +0530

    Seventh commit

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://appointment-bot-dxbng2rk3utxgvjcrkcthr.streamlit.app"],  
+    allow_origins=["*"],  
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
